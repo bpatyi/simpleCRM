2ac4c83ad39e78a3ea19c242d166f49fb5497286
Bence Patyi <patyi.bence.90@gmail.com>
2016-11-18 17:28:31 +0100

    add inbound contact tests

--- crm/tests/test_models.py
+++ crm/tests/test_models.py
@@ -22,7 +22,6 @@ from crm.models import (
 class IndividualTester(TestCase):
 
     def setUp(self):
-        self.individual = None
         self.individual = Individual.objects.create(
             gender='M',
             title='dr',
@@ -88,4 +87,99 @@ class IndividualTester(TestCase):
         self.assertIsInstance(email, IndividualEmail)
         self.assertEqual(email.individual.id, self.individual.id)
         self.assertEqual(email.email, 'joe.don@test.com')
-        
+
+
+class InboundContactTester(TestCase):
+
+    def setUp(self):
+        self.individual = Individual.objects.create(
+            gender='M',
+            title='dr',
+            first_name='Joe',
+            last_name='Don',
+            family_status='S',
+            education_level='E'
+        )
+        self.source_type = SourceType.objects.create(
+            name="Test Source Type"
+        )
+        self.source = Source.objects.create(
+            name="Test Source",
+            type_of_source=self.source_type
+        )
+
+        self.inbound_contact = InboundContact.objects.create(
+            individual=self.individual,
+            gender='M',
+            title='dr',
+            first_name='Joe',
+            last_name='Don',
+            family_status='S',
+            education_level='E',
+            source=self.source,
+            searchable_channels=['M', 'E', 'P']
+        )
+
+    def test_inbound_contact(self):
+        self.assertIsInstance(self.inbound_contact, InboundContact)
+        self.assertIsInstance(self.individual, Individual)
+        self.assertIsInstance(self.source, Source)
+        self.assertIsInstance(self.source_type, SourceType)
+
+        self.assertEqual(self.inbound_contact.individual.id, self.individual.id)
+        self.assertEqual(self.inbound_contact.source.id, self.source.id)
+        self.assertEqual(self.inbound_contact.source.type_of_source.id, self.source_type.id)
+
+        self.assertEqual(self.inbound_contact.gender, 'M')
+        self.assertEqual(self.inbound_contact.title, 'dr')
+        self.assertEqual(self.inbound_contact.first_name, 'Joe')
+        self.assertEqual(self.inbound_contact.last_name, 'Don')
+        self.assertEqual(self.inbound_contact.family_status, 'S')
+        self.assertEqual(self.inbound_contact.education_level, 'E')
+        self.assertEqual(self.inbound_contact.searchable_channels, ['M', 'E', 'P'])
+
+    def test_inbound_contact_address(self):
+        address = InboundContactAddress.objects.create(
+            inbound_contact=self.inbound_contact,
+            country='HU',
+            postal_code='1061',
+            county='Budapest',
+            locality='Budapest',
+            route='Király utca',
+            street_number='46',
+            formatted_address='Budapest, Király u. 46, 1061 Hungary'
+        )
+
+        self.assertIsInstance(address, InboundContactAddress)
+        self.assertEqual(address.inbound_contact.id, self.inbound_contact.id)
+        self.assertEqual(address.country, 'HU')
+        self.assertEqual(address.postal_code, '1061')
+        self.assertEqual(address.county, 'Budapest')
+        self.assertEqual(address.locality, 'Budapest')
+        self.assertEqual(address.route, 'Király utca')
+        self.assertEqual(address.street_number, '46')
+        self.assertEqual(address.formatted_address, 'Budapest, Király u. 46, 1061 Hungary')
+
+    def test_inbound_contact_phone(self):
+        phone = InboundContactPhone.objects.create(
+            inbound_contact=self.inbound_contact,
+            country='HU',
+            number='+36701234567',
+            type='mobile'
+        )
+
+        self.assertIsInstance(phone, InboundContactPhone)
+        self.assertEqual(phone.inbound_contact.id, self.inbound_contact.id)
+        self.assertEqual(phone.country, 'HU')
+        self.assertEqual(phone.number, '+36701234567')
+        self.assertEqual(phone.type, 'mobile')
+
+    def test_inbound_contact_email(self):
+        email = InboundContactEmail.objects.create(
+            inbound_contact=self.inbound_contact,
+            email='joe.don@test.com'
+        )
+
+        self.assertIsInstance(email, InboundContactEmail)
+        self.assertEqual(email.inbound_contact.id, self.inbound_contact.id)
+        self.assertEqual(email.email, 'joe.don@test.com')
