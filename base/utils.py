4025de1c2733e995f69f196eca11402561603b5c
Bence Patyi <patyi.bence.90@gmail.com>
2016-11-18 16:29:08 +0100

    refact: google maps geocoder obj

--- base/utils.py
+++ base/utils.py
@@ -1,45 +1,36 @@
-import os
 import googlemaps
 
 from django.conf import settings
 
+from base.models import GeoCoderLog
 
 client = googlemaps.Client(key=settings.GOOGLE_MAP_KEY)
 
 
-class GeoCoder(object):
-
-    @staticmethod
-    def geocode(address):
-        return GoogleAddress(address)
-
-
 class GoogleAddress(object):
-    country = None
-    postal_code = None
-    postal_code_suffix = None
-    county = None
-    administrative_area = None
-    locality = None
-    district = None
-    route = None
-    street_number = None
-    type = None
-    formatted_address = None
-    latitude = None
-    longitude = None
 
-    is_valid = False
+    def __init__(self):
+        self.country = None
+        self.postal_code = None
+        self.postal_code_suffix = None
+        self.county = None
+        self.administrative_area = None
+        self.locality = None
+        self.district = None
+        self.route = None
+        self.street_number = None
+        self.type = None
+        self.formatted_address = None
+        self.latitude = None
+        self.longitude = None
 
-    def __init__(self, address):
-        response = client.geocode(address)
+        self.is_valid = False
+        self.is_cleansed = False
 
+    def process_response(self, response):
         if not response:
             return
 
-        self.process_response(response)
-
-    def process_response(self, response):
         data = response[0]
         address_components = data.get('address_components')
 
@@ -72,6 +63,49 @@ class GoogleAddress(object):
         self.type = data['types'][0]
 
         self.is_valid = True
+        self.is_cleansed = True
 
-    def is_valid_address(self):
+        return self
+
+    def is_valid(self):
         return self.is_valid
+
+
+class GeoCoder(object):
+
+    def __init__(self):
+        self.__google_address = GoogleAddress()
+        self.__error = None
+
+    def geocode(self, address):
+
+        if GeoCoderLog.get_limit() == 0:
+            self.error = 'You used all your google maps option.'
+            return
+
+        try:
+            GeoCoderLog.update_today_number_of_request()
+            response = client.geocode(address)
+        except googlemaps.exceptions.HTTPError as e:
+            self.__error = e
+            return
+        except googlemaps.exceptions.ApiError as e:
+            self.__error = e
+            return
+        except googlemaps.exceptions.TransportError as e:
+            self.__error = e
+            return
+        except googlemaps.exceptions.Timeout as e:
+            self.__error = e
+            return
+
+        self.__google_address.process_response(response)
+
+    def get_address(self):
+        return self.__google_address
+
+    def has_error(self):
+        return self.__error is not None
+
+    def get_error(self):
+        return self.__error
