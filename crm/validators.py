6ae5d55173c6eb912019f18c407400d2cf2847a2
Bence Patyi <patyi.bence.90@gmail.com>
2016-11-18 16:27:32 +0100

    refact crm validators functions

--- crm/validators.py
+++ crm/validators.py
@@ -4,60 +4,44 @@ from django.core.exceptions import ValidationError
 from django.core.validators import validate_email as dj_validate_email
 
 from base.utils import GeoCoder
-from base.models import GeoCoderLog
 from crm.models import IndividualPhone
 
 
 def validate_address(data):
-    if GeoCoderLog.get_limit() == 0:
-        data['is_valid'] = False
-        data['is_cleansed'] = False
-        return data
-
     address = ' '.join([
         data.get('locality'),
         data.get('route'),
-        str(data.get('street_number')),
+        str(data.get('street_number', '')),
         data.get('postal_code'),
         data.get('country')
     ])
 
-    geocode = GeoCoder.geocode(address)
-    GeoCoderLog.update_today_number_of_request()
+    geocoder = GeoCoder()
+    geocoder.geocode(address)
+    address = geocoder.get_address()
 
-    if geocode.is_valid_address():
-        data['country'] = geocode.country
-        data['postal_code'] = geocode.postal_code
-        data['postal_code_suffix'] = geocode.postal_code_suffix
-        data['county'] = geocode.county
-        data['administrative_area'] = geocode.administrative_area
-        data['locality'] = geocode.locality
-        data['district'] = geocode.district
-        data['route'] = geocode.route
-        data['street_number'] = geocode.street_number
-        data['final_type'] = geocode.type
-        data['formatted_address'] = geocode.formatted_address
-        data['latitude'] = geocode.latitude
-        data['longitude'] = geocode.longitude
-
-        data['is_valid'] = True
-
-    data['is_cleansed'] = True
-
-    return data
-
-
-def validate_phone(data):
-    try:
-        number = phonenumbers.parse(data.get('number'), data.get('country'))
-    except phonenumbers.NumberParseException:
+    if geocoder.has_error():
         data['is_valid'] = False
         data['is_cleansed'] = False
         return data
+    elif not address.is_valid():
+        data['is_valid'] = False
+        data['is_cleansed'] = True
+        return data
+
+    return address.__dict__
+
+
+def validate_phone(data):
+    data['is_valid'] = False
+    data['is_cleansed'] = False
+
+    try:
+        number = phonenumbers.parse(data.get('number'), data.get('country'))
+    except phonenumbers.NumberParseException:
+        return data
 
     if not phonenumbers.is_valid_number(number):
-        data['is_valid'] = False
-        data['is_cleansed'] = False
         return data
 
     data['number'] = phonenumbers.format_number(
@@ -66,7 +50,7 @@ def validate_phone(data):
     )
 
     data['type'] = IndividualPhone.get_type(
-        phonenumbers.number_type(data.get('number'))
+        phonenumbers.number_type(number)
     )
     data['is_valid'] = True
     data['is_cleansed'] = True
